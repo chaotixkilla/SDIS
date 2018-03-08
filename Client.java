c27b39bb07ab9bd08a53608160e09a59d5325cbb
up201406136@FEUPSIG.FE.UP.PT <up201406136@FEUPSIG.FE.UP.PT>
2018-03-08 11:25:09 +0000

    aula2

--- Client.java
+++ Client.java
@@ -1,56 +1,44 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.regex.Pattern;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
 
 public class Client {
-	public static void main(String[] args) throws IOException{
-		String message;
-		String command = args[2];
-		if(!command.equals("register") && !command.equals("lookup")){
-			System.out.println("Unknown command: " + command);
+
+	public static void main(String[] args) throws IOException, SocketException {
+		if (args.length < 4 || args.length > 5) {
+			System.out.println("Invalid number of args!");
 			return;
 		}
 		
-		//check commands argument length
-		if(command.equals("register")){
-			if(args.length != 5){
-				System.out.println("1Invalid number of arguments for that command");
-			}
-		}
-		if(command.equals("lookup")){
-			if(args.length != 4){
-				System.out.println("2Invalid number of arguments for that command");
-			}
-		}
-		
-		//checking if the plate number is correct
+		String mcastAddress = args[0];
+		String mcastPort = args[1];
+		String operation = args[2];
 		String plateNumber = args[3];
-		boolean validPlate = Pattern.matches("^[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}$", plateNumber);
 		
-		if(!validPlate){
-			System.out.println("That plate number format is wrong!");
-			return;
+		if (operation.equals("register")) {
+			String ownerName = args[4];
 		}
 		
-		//get name and create string message
-		if(command.equals("register")){
-			String name = args[4];
-			message = command + ' ' + plateNumber + ' ' + name; 
+		InetAddress addr = InetAddress.getByName(mcastAddress);
+		int port = Integer.parseInt(mcastPort);
+		MulticastSocket socket = new MulticastSocket(port);
+		
+		socket.joinGroup(addr);
+		
+		for (int i = 0; i < 5; i++) {
+			byte[] buf = new byte[256];
+			DatagramPacket teste = new DatagramPacket(buf, buf.length);
+			socket.receive(teste);
+			
+			String msg = new String(buf, 0, buf.length);
+			msg = msg.trim();
+			System.out.println(msg);
 		}
-		else{
-			message = command + ' ' + plateNumber;
-		}
 		
-		System.out.println("Sent message: " + message);
-		
-		//create message
-		/*byte[] buffer = message.getBytes();
-		
-		//create socket
-		DatagramSocket socket = new DatagramSocket();
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-		
-		socket.send(packet);*/
+		socket.leaveGroup(addr);
 	}
+
 }
--- Server.java
+++ Server.java
@@ -1,5 +1,45 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Server {
-	public static void main(String[] args){
+
+	public static void main(String[] args) throws UnknownHostException, SocketException {
+		if (args.length != 3) {
+			System.out.println("Invalid number of args!");
+			return;
+		}
 		
+		String servicePortString = args[0];
+		String mcastAddress = args[1];
+		String mcastPort = args[2];
+		boolean flag = true;
+		
+		DatagramSocket serverSocket = new DatagramSocket();
+		int i = 1;
+		
+		while (flag) {
+			try{
+				byte[] buf = new byte[256];
+				
+				InetAddress addr = InetAddress.getByName(mcastAddress);
+				int port = Integer.parseInt(mcastPort);
+				
+				String teste = "teste " + i;
+				
+				DatagramPacket packet;
+				packet = new DatagramPacket(teste.getBytes(), teste.getBytes().length, addr, port);
+				serverSocket.send(packet);
+				i++;
+			}
+			catch(IOException e){
+				flag = false;
+			}
+		}
+		
+		serverSocket.close();
 	}
 }
